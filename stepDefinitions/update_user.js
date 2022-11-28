61a49c6f58b4361fed3be0a9710d1f4a6fa4f366
busrataltekin <busrataltekin@gmail.com>
2022-11-28 23:24:12 +0300

    [Busra] add firstName & lastName validations

--- stepDefinitions/update_user.js
+++ stepDefinitions/update_user.js
@@ -1,11 +1,31 @@
 
-const {Given} = require("@cucumber/cucumber")
+const {Given, Then} = require("@cucumber/cucumber")
 const  setPutRequestBody = require ("../support/action/setPutRequestBody.js")
 const elementMapping = require("../support/elementMapping.json")
+const checkElementIsEqual = require("../support/check/checkElementIsEqual")
+const checkElementIsNotEqual = require("../support/check/checkElementIsNotEqual")
+
+
 
 
 Given('Busra has updated the information with {string} and {string}', async function (firstName, lastName) {
 
     reqBody =await setPutRequestBody.call(this,elementMapping[firstName],elementMapping[lastName])
+
+});
+Then('Busra should see the {string} in response', async function (newFirstName) {
+
+    await checkElementIsEqual.call(this,resBody.data.firstName,elementMapping[newFirstName])
+
+});
+Then('Busra should not see the {string} in response', async function (notExpectedName) {
+
+    if(notExpectedName === "firstName")
+
+    await checkElementIsNotEqual.call(this,resBody.data.firstName,elementMapping[notExpectedName])
+
+    else
+
+        await checkElementIsNotEqual.call(this,resBody.data.lastName,elementMapping[notExpectedName])
 
 });
