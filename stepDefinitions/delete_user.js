8b4d80d251dcf011670159ab9788123b71bce0c5
busrataltekin <busrataltekin@gmail.com>
2022-11-28 23:23:20 +0300

    [Busra] add check element unequal func

--- stepDefinitions/delete_user.js
+++ stepDefinitions/delete_user.js
@@ -2,6 +2,7 @@ const {When,Then} = require("@cucumber/cucumber")
 const elementMapping = require("../support/elementMapping.json")
 const axios = require("axios");
 const {expect} = require("chai");
+const checkElementIsNotEqual = require("../support/check/checkElementIsNotEqual")
 
 
 When('Busra sends the delete request', async function () {
@@ -13,5 +14,10 @@ Then('Busra should not see {string}', async function (user) {
 
 
     expect(resBody.data[0].id).to.have.not.equal(elementMapping[user])
+
+});
+Then('Busra should not see user after deleting', async function () {
+
+    await checkElementIsNotEqual.call(this,res.data,[])
 
 });
